e6728d4fd97eaa97b6a58e9d8a9aed42ab1ebc8d
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2025-01-03 11:46:13 +0530

    Update afk.py
    Signed-off-by: Frozen owner  <156554454+prashantsahlot@users.noreply.github.com>

--- FallenRobot/modules/afk.py
+++ FallenRobot/modules/afk.py
@@ -146,7 +146,7 @@ When marked as AFK, any mentions will be replied to with a message to say you're
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk, run_async=True)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
-    Filters.regex(r"^(?i)brb(.*)$"), afk, friendly="afk", run_async=True
+    Filters.regex(r"(?i)^brb(.*)$"), afk, friendly="afk", run_async=True
 )
 NO_AFK_HANDLER = MessageHandler(
     Filters.all & Filters.chat_type.groups, no_longer_afk, run_async=True
