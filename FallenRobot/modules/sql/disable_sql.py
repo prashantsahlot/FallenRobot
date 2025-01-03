0305218eabd938c7c603b2f453adea7d4c16b30d
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2025-01-03 11:31:45 +0530

    Update disable_sql.py
    Signed-off-by: Frozen owner  <156554454+prashantsahlot@users.noreply.github.com>

--- FallenRobot/modules/sql/disable_sql.py
+++ FallenRobot/modules/sql/disable_sql.py
@@ -1,60 +1,59 @@
 import threading
+
 from sqlalchemy import Column, String, UnicodeText, distinct, func
+
 from FallenRobot.modules.sql import BASE, SESSION
 
 
 class Disable(BASE):
     __tablename__ = "disabled_commands"
-    chat_id = Column(String(14), primary_key=True, nullable=False)
-    command = Column(String(255), primary_key=True, nullable=False)
+    chat_id = Column(String(14), primary_key=True)
+    command = Column(UnicodeText, primary_key=True)
 
     def __init__(self, chat_id, command):
         self.chat_id = chat_id
         self.command = command
 
     def __repr__(self):
-        return f"Disabled cmd {self.command} in {self.chat_id}"
+        return "Disabled cmd {} in {}".format(self.command, self.chat_id)
 
 
+Disable.__table__.create(checkfirst=True)
 DISABLE_INSERTION_LOCK = threading.RLock()
+
 DISABLED = {}
 
 
 def disable_command(chat_id, disable):
     with DISABLE_INSERTION_LOCK:
-        try:
-            disabled = SESSION.query(Disable).filter_by(chat_id=str(chat_id), command=disable).first()
+        disabled = SESSION.query(Disable).get((str(chat_id), disable))
 
-            if not disabled:
-                DISABLED.setdefault(str(chat_id), set()).add(disable)
-                new_disable = Disable(chat_id=str(chat_id), command=disable)
-                SESSION.add(new_disable)
-                SESSION.commit()
-                return True
-            return False
-        except Exception as e:
-            SESSION.rollback()
-            raise e
-        finally:
-            SESSION.close()
+        if not disabled:
+            DISABLED.setdefault(str(chat_id), set()).add(disable)
+
+            disabled = Disable(str(chat_id), disable)
+            SESSION.add(disabled)
+            SESSION.commit()
+            return True
+
+        SESSION.close()
+        return False
 
 
 def enable_command(chat_id, enable):
     with DISABLE_INSERTION_LOCK:
-        try:
-            disabled = SESSION.query(Disable).filter_by(chat_id=str(chat_id), command=enable).first()
+        disabled = SESSION.query(Disable).get((str(chat_id), enable))
 
-            if disabled:
-                DISABLED.get(str(chat_id), set()).discard(enable)
-                SESSION.delete(disabled)
-                SESSION.commit()
-                return True
-            return False
-        except Exception as e:
-            SESSION.rollback()
-            raise e
-        finally:
-            SESSION.close()
+        if disabled:
+            if enable in DISABLED.get(str(chat_id)):  # sanity check
+                DISABLED.setdefault(str(chat_id), set()).remove(enable)
+
+            SESSION.delete(disabled)
+            SESSION.commit()
+            return True
+
+        SESSION.close()
+        return False
 
 
 def is_command_disabled(chat_id, cmd):
@@ -68,9 +67,6 @@ def get_all_disabled(chat_id):
 def num_chats():
     try:
         return SESSION.query(func.count(distinct(Disable.chat_id))).scalar()
-    except Exception as e:
-        SESSION.rollback()
-        raise e
     finally:
         SESSION.close()
 
@@ -78,45 +74,32 @@ def num_chats():
 def num_disabled():
     try:
         return SESSION.query(Disable).count()
-    except Exception as e:
-        SESSION.rollback()
-        raise e
     finally:
         SESSION.close()
 
 
 def migrate_chat(old_chat_id, new_chat_id):
     with DISABLE_INSERTION_LOCK:
-        try:
-            chats = SESSION.query(Disable).filter_by(chat_id=str(old_chat_id)).all()
-            for chat in chats:
-                chat.chat_id = str(new_chat_id)
-                SESSION.add(chat)
+        chats = SESSION.query(Disable).filter(Disable.chat_id == str(old_chat_id)).all()
+        for chat in chats:
+            chat.chat_id = str(new_chat_id)
+            SESSION.add(chat)
 
-            if str(old_chat_id) in DISABLED:
-                DISABLED[str(new_chat_id)] = DISABLED.pop(str(old_chat_id), set())
+        if str(old_chat_id) in DISABLED:
+            DISABLED[str(new_chat_id)] = DISABLED.get(str(old_chat_id), set())
 
-            SESSION.commit()
-        except Exception as e:
-            SESSION.rollback()
-            raise e
-        finally:
-            SESSION.close()
+        SESSION.commit()
 
 
 def __load_disabled_commands():
     global DISABLED
     try:
-        Disable.__table__.create(bind=SESSION.get_bind(), checkfirst=True)  # Ensure table creation
         all_chats = SESSION.query(Disable).all()
         for chat in all_chats:
             DISABLED.setdefault(chat.chat_id, set()).add(chat.command)
-    except Exception as e:
-        SESSION.rollback()
-        raise e
+
     finally:
         SESSION.close()
 
 
-# Initialize disabled commands on load
 __load_disabled_commands()
